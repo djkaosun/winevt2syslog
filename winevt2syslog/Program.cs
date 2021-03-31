b61c93432835bab56a42b0947493aaf909b87bc6
DJ_Kaosun <djkaosun@gmail.com>
2021-03-31 10:49:24 +0900

    initial commit

---
+++ winevt2syslog/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winevt2syslog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
